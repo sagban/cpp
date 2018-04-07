e4d012ed27f62f9a8489a1db78300d092d21aaa6
Sagar Bansal <sagarbansal099@gmail.com>
2018-04-07 22:24:33 +0530

    sol to qal 2018

---
+++ CodeJam18 QA/cubicUFO.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    for(int i=0;i<T;i++){
+        float
+    }
+}
+
+
+
+//
+// Created by Sagar Bansal on 4/7/18.
+//
+
